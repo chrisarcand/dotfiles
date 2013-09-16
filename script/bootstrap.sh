6c9cedfa41d8d32c19b14ed2d94300d99bd36f31
ChrisArcand <chris@chrisarcand.com>
2013-09-15 23:20:29 -0500

    Fixed stuff

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -36,7 +36,7 @@ setup_gitconfig () {
     user ' - What is your github author email?'
     read -e git_authoremail
 
-    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" git/gitconfig.symlink.example > git/gitconfig.symlink
+    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" git/gitconfig.symlink.template > git/gitconfig.symlink
 
     success 'gitconfig'
   fi
