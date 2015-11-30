557577dd6c07ab42fd6b91cf14d4db64639c08c3
chrisarcand <chris@chrisarcand.com>
2015-11-30 16:30:02 -0500

    Add rbenv installer

---
+++ ruby/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo ""
+printf "\r  [ \033[00;34m..\033[0m ] starting rbenv/ruby-build installation\n"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # OSX
+  brew install rbenv ruby-build
+elif [[ ("$OSTYPE" == "linux-gnu") ]]; then
+  git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+else
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  echo "FAILED!"
+  echo "No config for the current OS/distribution found."
+  echo "Add installation steps to $DIR/$(basename $0)"
+  exit 1
+fi
