33ce735cf952b683907051472d14878bdf0934aa
chrisarcand <chris@chrisarcand.com>
2015-11-30 14:22:47 -0500

    Only use gvim if Fedora

--- vim/aliases.zsh
+++ vim/aliases.zsh
@@ -1,3 +1,5 @@
 # I use vim from Homebrew in OSX; which will get appended to the front of $PATH
 # For Fedora/RHEL/CentOS, I use gvim through vim-X11.
-alias vim='gvim -v'
+if [[ ("$OSTYPE" == "linux-gnu") && (-f "/etc/fedora-release" || -f "/etc/redhat-release") ]]; then
+  alias vim='gvim -v'
+fi
