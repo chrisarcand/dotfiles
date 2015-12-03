5b6de467a8f83a65456d6a431f8d966f8e71d3dc
chrisarcand <chris@chrisarcand.com>
2015-12-03 11:47:08 -0500

    Install Vundle and plugins with vim

--- vim/install.sh
+++ vim/install.sh
@@ -6,6 +6,8 @@ printf "\r  [ \033[00;34m..\033[0m ] starting vim installation\n"
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # OSX
   brew install vim
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall! +qall
 elif [[ ("$OSTYPE" == "linux-gnu") && (-f "/etc/fedora-release" || -f "/etc/redhat-release") ]]; then
   # Fedora, RHEL, or CentOS
   # vim in Fedora is compiled without any X support in order to minimize the number of dependencies it has.
