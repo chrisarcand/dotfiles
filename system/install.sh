a4399b9d1b5cf933f57c8d1d3a15429dbc33646f
chrisarcand <chris@chrisarcand.com>
2015-12-03 12:43:39 -0500

    Add ShiftIt to installer

--- system/install.sh
+++ system/install.sh
@@ -6,4 +6,5 @@ printf "\r  [ \033[00;34m..\033[0m ] installing misc system packages\n"
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # OSX
   brew cask install seil # Key mapper; Remap CapsLock->ESC with this
+  brew cask install shiftit # For arranging windows Windows/Fedora style (opt+arrows for half screen, etc)
 fi
