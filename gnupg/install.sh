1f8255ee779d8404b5802c090ff6716c40da817b
Chris Arcand <chris@chrisarcand.com>
2016-04-06 10:18:06 -0400

    Use gnupg/gpg-agent to always sign commits
    Because hey, everyone's going to get on this train with
    GitHub showing verified commits now.

---
+++ gnupg/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo ""
+printf "\r  [ \033[00;34m..\033[0m ] installing gnupg\n"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  brew install gnupg gpg-agent
+elif [[ ("$OSTYPE" == "linux-gnu") && (-f "/etc/fedora-release" || -f "/etc/redhat-release") ]]; then
+  printf "\r  [ \033[00;34m..\033[0m ] gnupg should already been installed on this distro, skipping...\n"
+fi
+
+
+printf "\r  [ \033[00;34m..\033[0m ] WARNING: I AM LAZY AND HAVE NOT AUTOMATED THE CONFIG OF GNUPG AND GPG-AGENT\n"
+printf "\r  [ \033[00;34m..\033[0m ] You should set .gnupg/gpg.conf to use-agent and maybe a custom timeout in gpg-agent.conf\n"
