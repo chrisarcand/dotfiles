5f5c54410e3fa00d0eda8e4482e14c5390068874
Chris Arcand <chris@chrisarcand.com>
2017-04-28 22:08:08 -0500

    Don't need these config opts and don't rehash

--- ruby/environment.zsh
+++ ruby/environment.zsh
@@ -1,6 +1,2 @@
-# Fixes Pry stupidity with history by using GNU readline instead of libedit
-# See https://github.com/guard/guard/wiki/Add-Readline-support-to-Ruby-on-Mac-OS-X
-export RUBY_CONFIGURE_OPTS=--with-readline-dir=`brew --prefix readline`
-
-eval "$(rbenv init - zsh)"
+# eval "$(rbenv init - zsh --no-rehash)"
 
