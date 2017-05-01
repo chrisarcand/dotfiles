c107a9c4a9810586e41952ea1de706a5c81dd424
Chris Arcand <chris@chrisarcand.com>
2017-04-30 22:14:51 -0500

    More Cleanup

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -2,19 +2,12 @@
 # Repo: https://github.com/andyfleming/oh-my-zsh
 # Direct Link: https://github.com/andyfleming/oh-my-zsh/blob/master/themes/af-magic.zsh-theme
 
-if [ $UID -eq 0 ]; then NCOLOR="red"; else NCOLOR="green"; fi
-local return_code="%(?..%{$fg[red]%}%? ↵%{$reset_color%})"
-
 # primary prompt
 PROMPT='$FG[032]%~\
 $(git_prompt_info) \
 $FG[105]%(!.#.»)%{$reset_color%} '
-PROMPT2='%{$fg[red]%}\ %{$reset_color%}'
-RPS1='${return_code}'
-
 
 # color vars
-eval my_gray='$FG[237]'
 eval my_orange='$FG[214]'
 
 # git settings
