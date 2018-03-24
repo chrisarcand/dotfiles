7d9683a73adf03352140a7c5a6e87c18fed6d849
Chris Arcand <chris@chrisarcand.com>
2018-03-24 11:47:05 -0500

    Obsolete option removal

--- gnupg/path.zsh
+++ gnupg/path.zsh
@@ -3,5 +3,5 @@ if [ -f ~/.gnupg/.gpg-agent-info ] && [ -n "$(pgrep gpg-agent)" ]; then
     source ~/.gnupg/.gpg-agent-info
     export GPG_AGENT_INFO
 else
-    eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
+    eval $(gpg-agent --daemon ~/.gnupg/.gpg-agent-info)
 fi
